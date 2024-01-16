866f4c4d0b2fed8a3d84a59163a0ef49ebfd2cae
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2024-01-16 20:47:56 +0100

    lavoro finito

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll('.lazy').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
+
 let carousels = ['#trending-carousel', '#watchagain-carousel', '#newreleases-carousel'];
 
 carousels.forEach((carousel) => {
